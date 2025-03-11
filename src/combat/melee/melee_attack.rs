6dab6f78a45f16c72623adc4775a8600bc8f600a
KaytonFletcher <kayton@fastmail.com>
2025-03-11 12:51:53 -0700

    Remove check for player and enemy in melee attack, we use collision layers so this isnt needed

--- src/combat/melee/melee_attack.rs
+++ src/combat/melee/melee_attack.rs
@@ -110,19 +110,15 @@ pub fn handle_melee_collisions(
         &mut ActiveMeleeAttack,
         &CollidingEntities,
     )>,
-    enemy_query: Query<&Enemy>,
-    player: Single<Entity, With<Player>>,
 ) {
-    let player_entity = player.into_inner();
-
     for (weapon_entity, melee_weapon, mut active_melee_attack, colliding_entities) in
         melee_query.iter_mut()
     {
         for &colliding_entity in colliding_entities.iter() {
-            if (enemy_query.contains(colliding_entity) || colliding_entity == player_entity)
-                && (!active_melee_attack
-                    .entities_damaged
-                    .contains(&colliding_entity))
+            // We only hit a given entity once per attack
+            if !active_melee_attack
+                .entities_damaged
+                .contains(&colliding_entity)
             {
                 commands.trigger_targets(
                     AttemptDamageEvent {
